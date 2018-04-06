567b29d23701e526ed14db8ccd3d582ee3c1296c
Mary Mbugua <marymbugua.nm@gmail.com>
2018-04-06 12:17:05 +0300

    creted news sources and news article class

--- app/models.py
+++ app/models.py
@@ -1,44 +1,26 @@
-class Movie:
+class Newssources:
     '''
-    movie class to define movie objects
+    newssources class  to define news sources objects
     '''
-
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
+    def  __init__(self,id,name,description,url,category,language,country):
         self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
+
+class Newsarticle:
+    '''
+    newsarticle class  to define news article objects
+    '''
+    def  __init__(self,source,author,title,description,url,urlToImage,publishedAt):
+        self.source = source
+        self.author = author
         self.title = title
-        self.overview = overview
-        self.poster = 'https://image.tmdb.org/t/p/w500/'+poster
-        self.vote_average = vote_average
-        self.vote_count = vote_count
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
 
-
-
-class Review:
-
-    all_reviews = []
-
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-
-    def save_review(self):
-        Review.all_reviews.append(self)
-
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-        
-    @classmethod
-    def get_reviews(cls,id):
-
-        response = []
-
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-
-        return response
